aa529e7f9a73dd54fed0346d7cdc01b881e52f7e
Tobias Seymour <tobiasseymour@tobiass-mbp.home>
2018-01-20 18:26:54 +0000

    green lights stay on

--- src/nav.js
+++ src/nav.js
@@ -6,6 +6,34 @@ export default class Nav extends React.Component {
         this.state = {};
     }
 
+    // navHandleClick(x) {
+    //     if (this.state.current != x) {
+    //         if (this.state.current) {
+    //             this.refs[this.state.current].classList.remove('dotHighlighted');
+    //         }
+    //         for (var val in this.refs) {
+    //             if (val == x) {
+    //                 this.setState({current: x}, () => {
+    //                     this.refs[x].classList.add('dotHighlighted');
+    //                 });
+    //             }
+    //         }
+    //     }
+    //
+    //     if (x === 'works') {
+    //         this.props.showWorks();
+    //     }
+    // }
+
+    worksHandleClick() {
+        if (this.refs['works'].classList.contains('dotHighlighted')) {
+            this.refs['works'].classList.remove('dotHighlighted');
+        } else {
+            this.refs['works'].classList.add('dotHighlighted');
+        }
+        this.props.showWorks()
+    }
+
     render() {
         return (
             <div className="nav">
@@ -17,9 +45,9 @@ export default class Nav extends React.Component {
                     <p>Lachlan Kosaniukinnes</p>
                 </div>
                 <div className="navLinks">
-                    <div className="linkWrapper"><div className="dot"></div><a id="lkOne" onClick={this.props.showWorks}>Works</a></div>
-                    <div className="linkWrapper"><div className="dot" id="dTwo"></div><a id="lkTwo" href="#">CV</a></div>
-                    <div className="linkWrapper" id="lkThree"><div className="dot" id="dThree"></div><a href="#">mail@tslki.com</a></div>
+                    <div className="linkWrapper"><div className="dot" ref="works"></div><a onClick={() => this.worksHandleClick()}>Works</a></div>
+                    <div className="linkWrapper"><div className="dot" ref="cv"></div><a onClick={() => this.cvHandleClick()}>CV</a></div>
+                    <div className="linkWrapper"><div className="dot" ref="email"></div><a onClick={() => this.emailHandleClick()}>mail@tslki.com</a></div>
                 </div>
             </div>
         )
